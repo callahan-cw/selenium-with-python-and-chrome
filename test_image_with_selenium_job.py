da03bc38d5e6038a8b1f0832ee13e115a7fc00f1
callahan-cw <55491136+callahan-cw@users.noreply.github.com>
2020-10-17 13:24:33 +0300

    Update test_image_with_selenium_job.py

--- test_image_with_selenium_job.py
+++ test_image_with_selenium_job.py
@@ -7,3 +7,13 @@ chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 driver = webdriver.Chrome(options=chrome_options)
 
+driver.get('www.google.com')
+source = driver.page_source
+if "I'm Feelling Lucky" in source:
+  print("Test passed")
+else:
+  print("Test failed")
+driver.close()
+
+
+
